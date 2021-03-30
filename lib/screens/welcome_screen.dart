a12719761e426091997ac96c9a040a2f09d7ea95
Leanne Sall <lsall1@my.bcit.ca>
2021-03-30 15:02:21 -0700

    base grabbing data complete

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:developer';
 
 class WelcomeScreen extends StatefulWidget {
   @override
@@ -7,12 +9,27 @@ class WelcomeScreen extends StatefulWidget {
 }
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
-  Future<http.Response> fetchAlbum() {
-    return http.get(Uri.https('jsonplaceholder.typicode.com', 'albums/1'));
+  void getData() async {
+    http.Response response = await http.get(
+        'https://api.coingecko.com/api/v3/coins/dopecoin?tickers=true&market_data=true&community_data=true&developer_data=true&sparkline=true');
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+
+      var decodedData = jsonDecode(data);
+
+      double currentPrice = decodedData['market_data']['current_price']['cad'];
+      String symbol = decodedData['symbol'];
+      String name = decodedData['name'];
+
+      print(currentPrice.toString());
+      print(symbol);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    getData();
+    return Scaffold(body: Container(child: Text('Hello World')));
   }
 }
