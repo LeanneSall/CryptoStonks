068354c3d7a3c6eba05ef47723d74fa5e71579f2
Leanne Sall <lsall1@my.bcit.ca>
2021-03-31 12:34:20 -0700

    Logout button functional

--- lib/screens/consume_api.dart
+++ lib/screens/consume_api.dart
@@ -1,3 +1,4 @@
+import 'package:cryptostonks/screens/welcome_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -33,45 +34,54 @@ class _ConsumeAPIState extends State<ConsumeAPI> {
     } catch (e) {}
   }
 
-  void getData() async {
-    http.Response response = await http.get(Uri.https(
-        "https://api.coingecko.com",
-        "/api/v3/coins/dopecoin?tickers=true&market_data=true&community_data=true&developer_data=true&sparkline=true"));
-
-    if (response.statusCode == 200) {
-      String data = response.body;
-
-      var decodedData = jsonDecode(data);
-
-      currentPrice = decodedData['market_data']['current_price']['cad'];
-      symbol = decodedData['symbol'];
-      name = decodedData['name'];
-    }
-  }
-
-  final appHeadBar = AppBar(
-    title: const Text('CryptoStonks'),
-    automaticallyImplyLeading: false,
-    elevation: 5.0,
-    leading: Icon(
-      Icons.menu,
-    ),
-    actions: <Widget>[
-      Padding(
-        padding: EdgeInsets.only(right: 20.0),
-        child: Icon(
-          Icons.search,
-          size: 26.0,
-        ),
-      ),
-      Padding(
-        padding: EdgeInsets.only(right: 20.0),
-        child: Icon(Icons.favorite),
-      ),
-    ],
-  );
-
   Widget build(BuildContext context) {
+    void getData() async {
+      http.Response response = await http.get(Uri.https(
+          "https://api.coingecko.com",
+          "/api/v3/coins/dopecoin?tickers=true&market_data=true&community_data=true&developer_data=true&sparkline=true"));
+
+      if (response.statusCode == 200) {
+        String data = response.body;
+
+        var decodedData = jsonDecode(data);
+
+        currentPrice = decodedData['market_data']['current_price']['cad'];
+        symbol = decodedData['symbol'];
+        name = decodedData['name'];
+      }
+    }
+
+    final appHeadBar = AppBar(
+      title: const Text('CryptoStonks'),
+      automaticallyImplyLeading: false,
+      elevation: 5.0,
+      actions: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(right: 20.0),
+          child: Icon(
+            Icons.search,
+            size: 26.0,
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(right: 20.0),
+          child: Icon(Icons.favorite),
+        ),
+        FlatButton.icon(
+          onPressed: () async {
+            try {
+              await _auth.signOut();
+              Navigator.pushNamed(context, WelcomeScreen.id);
+            } catch (e) {
+              print(e);
+            }
+          },
+          icon: Icon(Icons.person),
+          label: Text('logout'),
+        ),
+      ],
+    );
+
     return Scaffold(
       appBar: appHeadBar,
       body: Container(
