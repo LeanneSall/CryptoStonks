73327bebb4dacdbcdc5b98aa82f5c2d599ce9c96
Leanne Sall <lsall1@my.bcit.ca>
2021-03-30 20:20:50 -0700

    starting to add firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+
 import 'package:cryptostonks/screens/welcome_screen.dart';
 import 'package:cryptostonks/screens/consume_api.dart';
 import 'package:cryptostonks/screens/register.dart';
 import 'package:cryptostonks/screens/login.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+
 void main() {
   runApp(CryptoStonks());
 }
 
-class CryptoStonks extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,5 +28,31 @@ class CryptoStonks extends StatelessWidget {
           'register': (context) => Register(),
           'login': (context) => Login(),
         });
+  }
+}
+
+class CryptoStonks extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return exit(1);
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MyApp();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        //return Loading();
+      },
+    );
   }
 }
